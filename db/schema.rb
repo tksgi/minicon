0c6b89d598f77b66bd2a62afa4c28e48d3c9d136
Dennis Tanaka <me@denakitan.com>
2018-09-06 04:34:30 +0000

    Commit the schema that had the format changed in Rails 5.2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180125152003) do
+ActiveRecord::Schema.define(version: 2018_01_25_152003) do
 
   create_table "event_categories", force: :cascade do |t|
     t.string "name"
