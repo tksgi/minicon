2ce24d488b18d74e1d3903e750909062b5a4be06
Tetsu Kasugai <tksgi12@gmail.com>
2018-09-13 08:41:10 +0000

    initial setup for devise gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_13_054809) do
+ActiveRecord::Schema.define(version: 2018_09_13_083801) do
 
   create_table "event_categories", force: :cascade do |t|
     t.string "name"
@@ -29,6 +29,18 @@ ActiveRecord::Schema.define(version: 2018_09_13_054809) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["event_category_id"], name: "index_events_on_event_category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
